233176e090e5e0fc636df671fbbfc88924871a5f
kwamekert <kwamekert@gmail.com>
2021-02-25 15:53:10 +0000

    make it better

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -102,6 +102,7 @@ async function fetchFrontPage(req, res){
                     }
                 }
             })
+            
             let upComingEvents = await Event.findAll({
                 where: {
                     endDate: {
