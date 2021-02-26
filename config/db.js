f6e1c1482a2e0c34ed6de527f9d7fdf8e3f39fb0
kwamekert <kwamekert@gmail.com>
2021-02-26 09:49:33 +0000

    database config updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Sequelize  = require('sequelize') ;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  loggin: true,
+  logging: true,
   ssl: true
 }) // Example for postgres
 
