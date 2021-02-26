c6ebc259322dd8306446a6cee057dd5b0c6548f5
kwamekert <kwamekert@gmail.com>
2021-02-26 09:34:42 +0000

    database url updated

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize  = require('sequelize') ;
 
-const sequelize = new Sequelize('postgres://njeanhtgqiwrax:8899623aa5aabf4191a2d2c1cc1c4f2b7e3d669545ed8aeee0ca6eb7173e1343@ec2-52-203-27-62.compute-1.amazonaws.com:5432/deu7trsv9cgrg4') // Example for postgres
+const sequelize = new Sequelize(process.env.DATABASE_URL) // Example for postgres
 
 // const sequelize = new Sequelize(
 //     process.env.DATABASE,
